fadd8540b44cb7f7111f8bffa6f4bf134cf4bfa4
Olli Rissanen <olli.rissanen@helsinki.fi>
2013-10-05 17:44:01 -0400

    Added Vote and Comment, added foreign keys

--- postapp/models.py
+++ postapp/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Post(models.Model):
+	user = models.ForeignKey(User)
 	score = models.IntegerField()
+	content = models.TextField(max_length=1000, blank=False, null=False)
+
+class Vote(models.Model):
+	user = models.ForeignKey(User)
+	vote = models.BooleanField()
+
+class Comment(models.Model):
+	post = models.ForeignKey(Post)
 	content = models.TextField(max_length=1000, blank=False, null=False)
