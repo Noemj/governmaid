6107414eac864d6868733ab16dead46cfaebd35c
Olli Rissanen <olli.rissanen@helsinki.fi>
2013-10-05 18:47:46 -0400

    Added cityapp, added foreign key from Post to City

--- postapp/models.py
+++ postapp/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cityapp import City
 
 
 class Post(models.Model):
-	user = models.ForeignKey(User)
+	city = models.ForeignKey(City,related_name='posts')
+	user = models.ForeignKey(User,related_name='posts')
 	score = models.IntegerField()
 	content = models.TextField(max_length=1000, blank=False, null=False)
 
 class Vote(models.Model):
-	user = models.ForeignKey(User)
+	user = models.ForeignKey(User,related_name='votes')
 	vote = models.BooleanField()
 
 class Comment(models.Model):
-	post = models.ForeignKey(Post)
+	post = models.ForeignKey(Post,related_name='comments')
 	content = models.TextField(max_length=1000, blank=False, null=False)
