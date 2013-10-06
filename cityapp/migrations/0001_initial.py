7085305ca2b27aabfbe0fc794065bf7953a65d89
Olli Rissanen <olli.rissanen@helsinki.fi>
2013-10-05 20:57:31 -0400

    Added cityapp migrations

---
+++ cityapp/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'City'
+        db.create_table(u'cityapp_city', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+        ))
+        db.send_create_signal(u'cityapp', ['City'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'City'
+        db.delete_table(u'cityapp_city')
+
+
+    models = {
+        u'cityapp.city': {
+            'Meta': {'object_name': 'City'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['cityapp']
