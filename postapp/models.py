880a3bf1575c386522521d396573cd3bfdbdb21c
Olli Rissanen <olli.rissanen@helsinki.fi>
2013-10-05 20:26:56 -0400

    Corrected cityapp imports

--- postapp/models.py
+++ postapp/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cityapp import City
-
 
 class Post(models.Model):
-	city = models.ForeignKey(City,related_name='posts')
+	city = models.ForeignKey('cityapp.City',related_name='posts')
 	user = models.ForeignKey(User,related_name='posts')
 	score = models.IntegerField()
 	content = models.TextField(max_length=1000, blank=False, null=False)
